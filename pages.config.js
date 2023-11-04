6f7740fbcc040b1a019ca709b3c1fbc63d1a8a95
MaksimPyatak <108072564+MaksimPyatak@users.noreply.github.com>
2023-11-04 19:12:39 +0200

    Added match

--- pages.config.js
+++ pages.config.js
@@ -1,14 +1,18 @@
 import { resolve } from "path";
 
 const pages = [
-  {
-    name: "main",
-    path: resolve(__dirname, "index.html"),
-  },
-  {
-    name: "auth",
-    path: resolve(__dirname, "auth.html"),
-  },
+   {
+      name: "main",
+      path: resolve(__dirname, "index.html"),
+   },
+   {
+      name: "auth",
+      path: resolve(__dirname, "auth.html"),
+   },
+   {
+      name: "match",
+      path: resolve(__dirname, "match.html"),
+   },
 ];
 
 export default pages;

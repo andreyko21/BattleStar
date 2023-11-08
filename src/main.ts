fc9c85fa6f3c403cb516f5497d232163352a8cfd
Alex <alexandre.kovalchuk@gmail.com>
2023-11-09 00:32:46 +0200

    add calendar

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import '/src/styles/index.scss';
+// Calendar
+import AirDatepicker from "air-datepicker";
+let datepicker = new AirDatepicker<HTMLDivElement>("#calendar", {
+  startDate: "2023-11-08",
+  dateFormat: "MMMM",
+  range: true,
+});
+
+console.log(datepicker);

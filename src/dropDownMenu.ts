e6b4f7907ad67e28a7cc61c9a2d31d9b06f257c4
Alex <alexandre.kovalchuk@gmail.com>
2023-11-14 23:22:20 +0200

    add dropDown

---
+++ src/dropDownMenu.ts
@@ -0,0 +1,24 @@
+export function initDropDown() {
+  const headerUserInfoBtn = document.querySelector<HTMLButtonElement>(
+    ".header__user-info-button"
+  );
+  const headerNotionBtn = document.querySelector<HTMLButtonElement>(
+    ".header__notification-button"
+  );
+  const headerMessBtn = document.querySelector<HTMLButtonElement>('.header__messages-button')
+
+  if (headerUserInfoBtn || headerNotionBtn ||  headerMessBtn) {
+    headerUserInfoBtn.addEventListener("click", showDropDown);
+   //  headerNotionBtn.addEventListener("click", showDropDown);
+   //  headerMessBtn.addEventListener("click", showDropDown);
+  }
+
+  function showDropDown() {
+    const dropDownAccount = document.querySelector<HTMLDivElement>(".dropdown__account");
+    const dropDownNotion = document.querySelector<HTMLDivElement>('.dropdown__notification')
+    const dropDownMess = document.querySelector<HTMLDivElement>('.dropdown__messages')
+    dropDownAccount?.classList.toggle("dropdown__account_hide");
+    dropDownNotion?.classList.toggle("dropdown__notification_hide");
+    dropDownMess?.classList.toggle("dropdown__messages_hide")
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 // Calendar
 import AirDatepicker from "air-datepicker";
+import { initDropDown } from "./dropDownMenu";
+initDropDown()
+
+
+
 let datepicker = new AirDatepicker<HTMLDivElement>("#calendar", {
   startDate: "2023-11-08",
   dateFormat: "MMMM",
@@ -7,3 +12,9 @@ let datepicker = new AirDatepicker<HTMLDivElement>("#calendar", {
 });
 
 console.log(datepicker);
+
+new Swiper(".banner__swiper", {
+loop: true,
+})
+
+

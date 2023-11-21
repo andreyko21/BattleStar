d2ed2191e75ffc97973e11acb177c7c6d9b718a8
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-11-21 14:15:17 +0200

    fix search

--- src/teams.ts
+++ src/teams.ts
@@ -6,13 +6,29 @@ import { AllTeamsRequest, Team, TeamType,  } from './ts/types';
 const ENDPOINT = 'https://battle-star-app.onrender.com/graphql';
 
 
-async function getDataBaseTeams(filtering: String = "", sorting: String = "name") {
-  const { teams: { data } } = await request<AllTeamsRequest>(ENDPOINT, GetTeams, { "filtering": filtering ,"sorting": sorting}); 
-  $('.my-teams-section__teams-list').empty();
-  data.forEach((team) => {
-    createItemList(team);
-  })
-  console.log();
+/**
+ * Fetches teams from the database and updates the UI.
+ * @param {string} filtering - Filter criteria for team names.
+ * @param {string} sorting - Sorting criteria.
+ */
+async function getDataBaseTeams(filtering: string = "", sorting: string = "rating") {
+  try {
+    const { teams: { data } } = await request<AllTeamsRequest>(ENDPOINT, GetTeams, { filtering, sorting }); 
+    $('.my-teams-section__teams-list').empty();
+
+    if (data.length > 0) {
+      data.forEach(team => createItemList(team));
+    } else {
+      let message = `<p class="error-request">Команди, назва якої починається з "${filtering}" не знайдено</p>`;
+      if (filtering.length > 20) {
+        message = `<p class="error-request">Команди, назва якої починається з "${filtering.substring(0, 20)}..." не знайдено</p>`;
+      }
+      $('.my-teams-section__teams-list').append(message);
+    }
+  } catch (error) {
+    console.error('Error fetching teams:', error);
+    // Handle error appropriately
+  }
 }
 
 $('.teams-list-section__sorting-select').change(function () {

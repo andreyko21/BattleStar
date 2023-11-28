9e2d9e3859642adae7f507b6856d1ca480d75ac3
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-11-28 09:04:05 +0200

    fix pagination

--- src/ts/teams/pagination.ts
+++ src/ts/teams/pagination.ts
@@ -1,3 +1,4 @@
+import { event } from "jquery";
 import { Meta } from "../types";
 
 type PaginationType = {
@@ -16,26 +17,52 @@ class Pagination implements PaginationType {
     listBlock.on("click", ".page-pagination__button", (event) => {
       window.location.search = `?page=${event.currentTarget.id}`;
     });
+    listBlock.on("click", ".page-pagination__button-nav", (event) => {
+      console.log(window.location.search);
+      if (window.location.search.includes("?page=")) {
+        let page = Number(window.location.search.replace("?page=", ""));
+        if (event.currentTarget.id == "next") {
+          page++;
+        } else if (event.currentTarget.id == "prev") {
+          page--;
+        }
+        if (page > this.pageCount) {
+          page = this.pageCount;
+        } else if (page < 1) {
+          page = 1;
+        }
+        window.location.search = `?page=${page}`;
+      }
+
+      console.log(event.currentTarget.id);
+    });
   }
   render(): string {
     let buttonsTemplate = "";
-    for (let i: number = 1; i <= this.pageCount; i++) {
+    for (let i = 1; i <= this.pageCount; i++) {
       if (i == this.currentPage) {
         buttonsTemplate += `<button id="${i}" class="page-pagination__button page-pagination__button_active">${i}</button>`;
-      } else {
+      } else if (
+        i == 1 ||
+        i == this.pageCount ||
+        i == this.currentPage - 1 ||
+        i == this.currentPage + 1
+      ) {
         buttonsTemplate += `<button id="${i}" class="page-pagination__button">${i}</button>`;
+      } else if (i == this.currentPage - 2 || i == this.currentPage + 2) {
+        buttonsTemplate += `<p class="page-pagination__elips">...</p>`;
       }
     }
 
     if (this.pageCount > 0) {
       return `<div class="teams-list-section__teams-list-pagination page-pagination">
-  <button id="next" class="page-pagination__button-prev">
+  <button id="prev" class="page-pagination__button-nav page-pagination__button-prev">
     <svg class="page-pagination__button-prev-icon">
       <use xlink:href="src/images/sprite.svg#arrow-left"></use>
     </svg>
   </button>
   ${buttonsTemplate}
-  <button id="next" class="page-pagination__button-next">
+  <button id="next" class="page-pagination__button-nav page-pagination__button-next">
     <svg class="page-pagination__button-next-icon">
       <use xlink:href="src/images/sprite.svg#arrow-right"></use>
     </svg>

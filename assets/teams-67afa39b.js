f27c8aceae921a4e1bd610ff8ca54052955630b4
andreyko21 <andrey.zakharkiv.2001@gmail.com>
2023-12-24 15:33:41 +0200

    Updates

--- assets/teams-67afa39b.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";import{$ as r}from"./jquery-995cfbaf.js";import{H as m,A as o}from"./sidebar-f3222882.js";import"./sprite-3002caef.js";import"./dropDownMenu-8d97c7ef.js";import"./windowLocation-a7eb2864.js";import"./queries.graphql.d-f9acd4bb.js";r(document).ready(()=>{new m("#wrapper"),new o("sidebar","КОМАНДЫ")});
